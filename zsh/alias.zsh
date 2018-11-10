cced8643c96f46ea58c1fd990697b0c76fd33f2d
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2018-11-10 12:34:45 -0500

    Remove unused aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -28,16 +28,12 @@ alias fix_stty='stty sane'                  # fix_stty:     Restore terminal set
 alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}' # grep: excluse certain dirs
 mcd () { mkdir -p "$1" && cd "$1"; }        # mcd:          Makes new Dir and jumps inside
 trash () { command mv "$@" ~/.Trash ; }     # trash:        Moves a file to the MacOS trash
-alias DT='tee ~/Desktop/terminalOut.txt'    # DT:           Pipe content to file on MacOS Desktop
 
 # -------------------------------
 # FILE AND FOLDER MANAGEMENT
 # -------------------------------
 
 zipf () { zip -r "$1".zip "$1" ; }          # zipf:         To create a ZIP archive of a folder
-alias make1mb='mkfile 1m ./1MB.dat'         # make1mb:      Creates a file of 1mb size (all zeros)
-alias make5mb='mkfile 5m ./5MB.dat'         # make5mb:      Creates a file of 5mb size (all zeros)
-alias make10mb='mkfile 10m ./10MB.dat'      # make10mb:     Creates a file of 10mb size (all zeros)
 
 # extract:  Extract most know archives with one command
 # ---------------------------------------------------------
