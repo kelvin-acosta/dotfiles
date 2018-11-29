7c685745cecd2dab469e6b2934533e0d90aacaa8
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2018-11-28 23:34:48 -0500

    Specify Brewfile when using brew bundle

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set base directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( dirname "$DIR" )"
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -10,4 +14,4 @@ brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
-brew bundle
+brew bundle --file=$BASE_DIR/brew/Brewfile
