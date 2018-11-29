a5b743f76b3806ab65f8ba7f1f0ca572c37709ba
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2018-11-28 23:33:26 -0500

    Install xdebug from pecl

--- php/setup.sh
+++ php/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Install XDebug
+pecl update-channels
+pecl install xdebug
+
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
