18c14814ac8b24792fcaba6109490f998e27224e
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2018-11-28 23:35:22 -0500

    Run vim script & php script in install script

--- install.sh
+++ install.sh
@@ -10,7 +10,14 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Set Mac OS Default
 source $HOME/.dotfiles/macos/set-defaults.sh
 
+# Install Brew & Brewfile
 source $HOME/.dotfiles/brew/setup.sh
+
+# Setup Vim
+source $HOME/.dotfiles/vim/vim.sh
+
+# Setup PHP
+source $HOME/.dotfiles/php/setup.sh
 
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
@@ -23,3 +30,5 @@ ln -sfn $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -sfn $HOME/.dotfiles/git/.gitignore_global $HOME/.gitignore_global
 ln -sfn $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
 ln -sfn $HOME/.dotfiles/zsh/zshrc.zsh $HOME/.zshrc
+
+echo "Done"
