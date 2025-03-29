1381c4840042595822f29f03942ed5285001deef
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2025-03-29 11:35:10 -0400

    brew

--- zsh/.zshrc.d/brew.zsh
+++ zsh/.zshrc.d/brew.zsh
@@ -8,3 +8,5 @@ export HOMEBREW_PREFIX="/opt/homebrew";
 export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
