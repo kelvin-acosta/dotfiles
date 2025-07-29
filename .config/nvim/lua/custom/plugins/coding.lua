cefe39e2e936efab2e4f727837821f8fa5141400
Kelvin Acosta <16613817+kelvin-acosta@users.noreply.github.com>
2025-07-29 19:05:18 -0400

    blah

--- .config/nvim/lua/custom/plugins/coding.lua
+++ .config/nvim/lua/custom/plugins/coding.lua
@@ -253,15 +253,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'tflint',
-        'helm-ls',
-        'terraform-ls',
-        'hadolint',
-        'markdownlint',
-        'shfmt',
-        'gopls',
-        'goimports',
-        'ruby-lsp',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
